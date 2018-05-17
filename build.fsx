dc648734bd3809a3f3a8942db759507bdd794024
giacomociti <giacomociti@gmail.com>
2018-05-17 23:40:19 +0200

    Add demo post

---
+++ build.fsx
@@ -0,0 +1,12 @@
+﻿#load "./packages/FSharp.Formatting/FSharp.Formatting.fsx"
+open System.IO
+open FSharp.Literate
+
+// Return path relative to the current file location
+let relative subdir = Path.Combine(__SOURCE_DIRECTORY__, subdir)
+
+Literate.ProcessDirectory(
+  inputDirectory = relative "fsx", 
+  templateFile = relative "template.html",
+  lineNumbers = false,
+  outputDirectory = relative "_posts")
